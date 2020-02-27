a8d6512fb1d562a8d08f49c0be339e5f68b89264
Gabriel Alejandro <gabriel2477@gmail.com>
2020-02-27 16:37:54 -0300

    Composite

---
+++ Composite/src/main/java/com/MitoCode/Composite/Archivo.java
@@ -0,0 +1,16 @@
+package com.MitoCode.Composite;
+
+public class Archivo extends Nodo{
+	
+	public Archivo( String nombre) {
+		this.setTipoNodo( Nodo.ARCHIVO );
+		this.setNombre( nombre );
+	}
+	
+	@Override
+	public void mostrar() {
+		System.out.println( "Archivo: [" + this.getNombre() + "]" );
+		
+	}
+
+}
